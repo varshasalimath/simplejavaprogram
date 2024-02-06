b2bd70ed85d024ad48de533da80f6e7661c2f774
varsha salimath <89127077+varshasalimath@users.noreply.github.com>
2024-02-06 08:36:28 +0530

    Add files via upload

---
+++ simpleprogram.java
@@ -0,0 +1,6 @@
+class SimpleProgram{  
+    public static void main(String args[]){  
+     System.out.println("Hello  varsha here!");
+      System.out.println("This is a simple java project");
+    }  
+} 
